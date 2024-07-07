4f4254cf065e1995039774c4531496d924c80bf7
shaheer-ali-coder <130545001+shaheer-ali-coder@users.noreply.github.com>
2024-07-07 00:43:17 -0700

    d
    d

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ async function sendSMS(to, message) {
         console.log('Message sent successfully. SID:', result.sid);
         return result.sid;
     } catch (error) {
-        console.error('Error sending SMS:', error);
-        throw error;
+        console.error('Error sending SMS:');
+        // throw error;
     }
 }
 
@@ -44,7 +44,8 @@ async function sendEmail(to, subject, text) {
     from: 'benefitactivation@gmail.com', 
     to,
     subject, 
-    text, 
+        html: text, 
+
     
   };
 
@@ -61,6 +62,10 @@ async function sendEmail(to, subject, text) {
 app.use(cors());
 app.use(bodyParser.json());
 app.get('/ailplus', (req, res) => {
+    let absolutePath = path.join(__dirname, '/pages/ailplus.html');
+    res.sendFile(absolutePath);
+  })
+  app.get('/', (req, res) => {
     let absolutePath = path.join(__dirname, '/pages/ailplus.html');
     res.sendFile(absolutePath);
   })
@@ -84,6 +89,7 @@ app.get('/user', (req, res) => {
         res.status(404).send('No users found');
       } else {
         const lastUser = users[users.length - 1];
+        console.log(lastUser)
         res.json(lastUser);
       }
     } catch (parseError) {
@@ -118,7 +124,7 @@ app.get('/user', (req, res) => {
   app.post('/bot', async (req, res) => {
       // Assuming the received data is a JSON object
       const receivedData = req.body;
-    
+      console.log(receivedData)
       // Read existing data from data.json (if any)
       let existingData = [];
       try {
@@ -132,7 +138,7 @@ app.get('/user', (req, res) => {
     
       // Add the received data to the existing data array
       existingData[receivedData['name']] = receivedData
-    
+      // console.log(existingData)
       // Write the updated data array back to data.json
       fs.writeFileSync('data.json', JSON.stringify(existingData, null, 2), 'utf8');
     
@@ -144,72 +150,70 @@ app.get('/user', (req, res) => {
     })
 
 
-    app.post('/email', (req, res) => {
+    app.post('/email', async (req, res) => {
       const requestData = req.body;
-      const platform = requestData['platform']
-      const email = requestData['email']
-      const name = requestData['name']
-      const zoom_link = requestData['link']
-      const phonenumber = requestData['phonenumber']
-      const address = requestData['address']
-      let agent_name = "Anthony"
-      let agent_phone_number = "4079212467"
-      const time = requestData['time']
-        let data2 = new FormData
-        data2.append('name',"ailplus")
-        data2.append('source',"13")
-        data2.append('status',"8")
-        data2.append("title","ailplus Meeting")
-        data2.append('assigned','35')
-        data2.append('client_id',"0")
-        // data2.append('email',email)
-        // data2.append('phonenumber',phonenumber)
-        // data2.append('address',address)
-        // data2.append("city",requestData['city'])
-        // data2.append("country",requestData['country'])
-        let config = {
-          method: 'post',
-          maxBodyLength: Infinity,
-          url: 'http://aileadtransfer.com/api/leads',
-          headers: {
-            'authtoken': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiVGVzdCB1c2VyIiwibmFtZSI6IlRlc3QgdXNlciIsInBhc3N3b3JkIjpudWxsLCJBUElfVElNRSI6MTY5ODY2ODA4MH0.r2d9VgVbMe3-JaAfRlkXMN_llNsG3lSqxvSgFgeK53w',
-            'Cookie': 'csrf_cookie_name=ed67552cee6b74b726acb03704571d95; sp_session=b08c5d39e6d27f448d6c182d472e7424946f9df8',
-            ...data2.getHeaders()
-          },
-          data: data2
-        };
-        
-        axios.request(config)
-          .then((response) => {
-            console.log(JSON.stringify(response.data));
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-        // addLeadToCRM(data2)
-        let emailText = `
-        img src="https://aileadtransfers.com/ADS/Images/jobs.jpg" alt="Logo">
-        <h1>This is to present to you the AIEAP Health Discount Program, AIL Plus.</h1>
-        <p>I will be texting you until the meeting to be sure it goes off without a hitch.  We all lead such busy lives that we try and assist people in completing the important appointment they have setup. 
-
-        Just so you know ${agent_name}  is one of the Advisors who is high in demand because of her dedication to following through with her appointments.
-        
-        The Zoom link - ${zoom_link}
-        
-        This is her cell number in case of emergency -
-        
-        ${agent_phone_number}
-        
-        Please text back a C to confirm your attendance at this appointment.
-        `
-        sendEmail(email, "Confirmation | AILPLUS", emailText);
-        sendSMS(phonenumber, emailText)
-        .then(sid => console.log('Message SID:', sid))
-        .catch(err => console.error('Failed to send message:', err));
-              res.json({ message: 'Endpoint 1 POST request received', data: requestData });
-        
-      res.json({ message: 'Endpoint 1 POST request received', data: requestData });
-    });
+      const platform = requestData['platform'];
+      const email = requestData['email'];
+      const name = requestData['name'];
+      const zoom_link = requestData['link'];
+      const phonenumber = requestData['phonenumber'];
+      // const address = requestData['address'];
+      let agent_name = "Anthony";
+      let agent_phone_number = "4079212467";
+      const time = requestData['time'];
+  
+      try {
+          // Sending email
+          let emailText = `
+              <img src="https://aileadtransfers.com/ADS/Images/jobs.jpg" alt="Logo">
+              <h1>This is to present to you the AIEAP Health Discount Program, AIL Plus.</h1>
+              <p>I will be texting you until the meeting to be sure it goes off without a hitch.  We all lead such busy lives that we try and assist people in completing the important appointment they have setup. 
+  
+              Just so you know ${agent_name}  is one of the Advisors who is high in demand because of her dedication to following through with her appointments.
+  
+              The Zoom link - ${zoom_link}
+  
+              This is her cell number in case of emergency - ${agent_phone_number}
+  
+              Please text back a C to confirm your attendance at this appointment.
+              `;
+  
+          await sendEmail(email, "Confirmation | AILPLUS", emailText);
+  
+          // Sending SMS
+          await sendSMS(phonenumber, emailText);
+  
+          // Adding lead to CRM
+          let data2 = new FormData();
+          data2.append('name', "ailplus");
+          data2.append('source', "13");
+          data2.append('status', "8");
+          data2.append("title", "ailplus Meeting");
+          // data2.append('assigned', '35');
+          data2.append('client_id', "0");
+  
+          let config = {
+              method: 'post',
+              maxBodyLength: Infinity,
+              url: 'http://aileadtransfer.com/api/leads',
+              headers: {
+                  'authtoken': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiVGVzdCB1c2VyIiwibmFtZSI6IlRlc3QgdXNlciIsInBhc3N3b3JkIjpudWxsLCJBUElfVElNRSI6MTY5ODY2ODA4MH0.r2d9VgVbMe3-JaAfRlkXMN_llNsG3lSqxvSgFgeK53w',
+                  'Cookie': 'csrf_cookie_name=ed67552cee6b74b726acb03704571d95; sp_session=b08c5d39e6d27f448d6c182d472e7424946f9df8',
+                  ...data2.getHeaders()
+              },
+              data: data2
+          };
+  
+          await axios.request(config);
+  
+          // Responding to client
+          res.json({ message: 'Email sent and SMS sent successfully', data: requestData });
+      } catch (error) {
+          console.error('Error processing request:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+      }
+  });
+  
 
 
     app.listen(port, () => {
